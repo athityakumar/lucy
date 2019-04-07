efe09789c6b3e7c49f8b8ae3582c0df8aaf3763c
Athitya <athityakumar@gmail.com>
2019-04-07 16:31:48 +0530

    Adds pso.py mail file to temp commit

--- pso.py
+++ pso.py
@@ -7,31 +7,31 @@ class PSO:
         self.type = "pso"
 
     def propose_regions(self):
-        regions = PSO_Regions(self.img, popSize=200, maxIterations=1)
+        regions = PSO_Regions(self.img, popSize=200, maxIterations=5)
         proposals = regions.fetch_candidates(w=1.0, c1=1, c2=1)
 
         MAX_ASPECT_RATIO = [1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0]
 
-        # candidates = [proposal.to_tuple() for proposal in proposals]
+        candidates = [proposal.to_tuple() for proposal in proposals]
         # candidates = []
         # for proposal in proposals:
         #     if proposal.size() != 0:
         #         candidates.append(proposal.to_tuple())
 
-        for max_aspect_ratio in reversed(MAX_ASPECT_RATIO):
-            candidates = []
-            for proposal in proposals:
-                if not proposal.aspect_ratio():
-                    continue
+        # for max_aspect_ratio in reversed(MAX_ASPECT_RATIO):
+        #     candidates = []
+        #     for proposal in proposals:
+        #         if not proposal.aspect_ratio():
+        #             continue
 
-                print(proposal.aspect_ratio(), 1.0/proposal.aspect_ratio(), proposal.size())
+        #         print(proposal.aspect_ratio(), 1.0/proposal.aspect_ratio(), proposal.size())
 
-                if proposal.aspect_ratio() > max_aspect_ratio or (1/proposal.aspect_ratio() > max_aspect_ratio):
-                    continue
-                if proposal.size() < 60000:
-                    continue
+        #         if proposal.aspect_ratio() > max_aspect_ratio or (1/proposal.aspect_ratio() > max_aspect_ratio):
+        #             continue
+        #         if proposal.size() < 60000:
+        #             continue
 
-                # proposal.__repr__()
-                candidates.append(proposal.to_tuple())
+        #         # proposal.__repr__()
+        #         candidates.append(proposal.to_tuple())
 
         return(candidates)
